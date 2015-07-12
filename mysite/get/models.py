c410ea54da4271a3544fe8fd75a568f9275ea67d
pokhrel <saurabhpokhrel@gmail.com>
2015-07-12 21:42:07 +0545

    added newsevents db support

--- mysite/get/models.py
+++ mysite/get/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class NewsEvents(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=500)
+    date = models.DateTimeField('pub_date')
